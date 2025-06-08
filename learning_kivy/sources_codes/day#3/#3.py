63ba958e5ba2cf534448f1b6fab0b57d9648e013
veee <paulag10987@gmail.com>
2025-06-08 15:03:37 +0800

    Add files via upload

--- learning_kivy/sources_codes/day#3/#3.py
+++ learning_kivy/sources_codes/day#3/#3.py
@@ -1,1 +1,25 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+class Touch(Widget):
+    btn = ObjectProperty()
+
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse UP", touch)
+        self.btn.opacity = 1
+
+class MyApp(App):
+    def build(self):
+        return Touch()
+
+if __name__ == "__main__":
+    MyApp().run()
